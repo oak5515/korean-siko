01f349601cf0df7165eb8c47f79278bdf259aee8
oak5515 <oak5515@naver.com>
2020-09-19 19:49:52 +0900

    add clear command

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
-const { send } = require('process');
 const client = new Discord.Client();
-const token = process.env.token;
-const welcomeChannelName = "안녕하세요!";
-const byeChannelName = "안녕히가세요!";
-const welcomeChannelComment = "어서오세요!";
-const byeChannelComment = "안녕히가세요!";
+const token = process.argv.length == 2 ? process.env.token : '';
+const welcomeChannelName = "안녕하세요";
+const byeChannelName = "안녕히가세요";
+const welcomeChannelComment = "어서오세요.";
+const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
-    console.log('켰다.');
+  console.log('켰다.');
 });
 
 client.on("guildMemberAdd", (member) => {
@@ -32,64 +31,41 @@ client.on("guildMemberRemove", (member) => {
 client.on('message', (message) => {
   if(message.author.bot) return;
 
-  if(message.content == '시코야 현재 핑') {
-    return message.reply('지금 올라오는 메세지와 방금 입력하신 메세지의 간격이 현재 핑 상태입니다.');
+  if(message.content == 'ping') {
+    return message.reply('pong');
   }
 
-  //시코봇 기타 말
-  if(message.content == '시코야') {
-    return message.reply('부르셨습니까?');
-  }
-
-  if(message.content == '시코야 좋은 아침') {
-    return message.channel.send(`<@${message.author.id}>` + "님, 좋은 아침입니다.");
-  }
-
-  if(message.content == '시코야 안녕') {
-    return message.channel.send("안녕하십니까, " + `<@${message.author.id}>` + "님.")
-  }
-
-  if(message.content == '시코야 채아리') {
-    return message.channel.send('저를 만들어주신 창조주님이십니다.')
-  }
-
-  if(message.content == '시코야 김승뭔') {
-    return message.channel.send('창조주님의 의형제님을 말씀하시는지요?')
-  }
-
-  if(message.content == '시코야 연유병') {
-    return message.channel.send('창조주님의 의남매님을 말씀하시는지요?')
-  }
-
-  //시코봇 도움말
-  if(message.content == '시코야 자기소개') {
-    let img = 'https://cdn.discordapp.com/attachments/756513236823572493/756782002748915712/20200918_224442.png';
+  if(message.content == 'embed') {
+    let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
     let embed = new Discord.RichEmbed()
-      .setTitle('이름 : 코리안시코')
+      .setTitle('타이틀')
+      .setURL('http://www.naver.com')
+      .setAuthor('나긋해', img, 'http://www.naver.com')
       .setThumbnail(img)
       .addBlankField()
-      .addField('영문 이름', 'Korean Siko')
-      .addField('아버지', '코리안식스', true)
-      .addField('어머니', '시식둥이', true)
-      .addField('오빠', '애플민트둥이', true)
-      .addField('특이사항', '말투: 딱딱함\n창조주: 채아리\n간단소개말: 안녕하십니까. 시코봇입니다.\n')
+      .addField('Inline field title', 'Some value here')
+      .addField('Inline field title', 'Some value here', true)
+      .addField('Inline field title', 'Some value here', true)
+      .addField('Inline field title', 'Some value here', true)
+      .addField('Inline field title', 'Some value here1\nSome value here2\nSome value here3\n')
       .addBlankField()
       .setTimestamp()
-      .setFooter('Made by 채아리, Profile Picture by @__kkung__ CM', img)
+      .setFooter('나긋해가 만듬', img)
 
     message.channel.send(embed)
-  } else if(message.content == '시코야 도움말') {
+  } else if(message.content == 'embed2') {
     let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
     let commandList = [
-      {name: '시코야 현재 핑', desc: '현재 핑 체감'},
-      {name: '시코야 도움말', desc: '시코봇의 사용 방법을 안내해드립니다.'},
-      {name: '시코야 (할 말)', desc: '(할 말) 안에 다른 말을 집어넣어 시코봇의 반응을 볼 수 있습니다.'},
+      {name: 'ping', desc: '현재 핑 상태'},
+      {name: 'embed', desc: 'embed 예제1'},
+      {name: 'embed2', desc: 'embed 예제2 (help)'},
+      {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
     ];
     let commandStr = '';
     let embed = new Discord.RichEmbed()
-      .setAuthor('시코봇 도움말!', helpImg)
+      .setAuthor('Help of 콜라곰 BOT', helpImg)
       .setColor('#186de6')
-      .setFooter(`Made by 채아리, Profile Picture by @__kkung__ CM`)
+      .setFooter(`콜라곰 BOT ❤️`)
       .setTimestamp()
     
     commandList.forEach(x => {
@@ -101,10 +77,10 @@ client.on('message', (message) => {
     message.channel.send(embed)
   }
 
-  if(message.content.startsWith('시코야 전체공지')) {
+  if(message.content.startsWith('!전체공지')) {
     if(checkPermission(message)) return
     if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('시코야 전체공지'.length);
+      let contents = message.content.slice('!전체공지'.length);
       message.member.guild.members.array().forEach(x => {
         if(x.user.bot) return;
         x.user.send(`<@${message.author.id}> ${contents}`);
@@ -113,6 +89,43 @@ client.on('message', (message) => {
       return message.reply('공지를 전송했습니다.');
     } else {
       return message.reply('채널에서 실행해주세요.');
+    }
+  }
+
+  if(message.content.startsWith('!청소')) {
+    if(checkPermission(message)) return
+
+    var clearLine = message.content.slice('!청소 '.length);
+    var isNum = !isNaN(clearLine)
+
+    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
+      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
+      return;
+    } else if(!isNum) { // c @나긋해 3
+      if(message.content.split('<@').length == 2) {
+        if(isNaN(message.content.split(' ')[2])) return;
+
+        var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
+        var count = parseInt(message.content.split(' ')[2])+1;
+        const _limit = 10;
+        let _cnt = 0;
+
+        message.channel.fetchMessages({limit: _limit}).then(collected => {
+          collected.every(msg => {
+            if(msg.author.id == user) {
+              msg.delete();
+              ++_cnt;
+            }
+            return !(_cnt == count);
+          });
+        });
+      }
+    } else {
+      message.channel.bulkDelete(parseInt(clearLine)+1)
+        .then(() => {
+          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
+        })
+        .catch(console.error)
     }
   }
 });
@@ -135,6 +148,14 @@ function changeCommandStringLength(str, limitLen = 8) {
   }
 
   return tmp;
+}
+
+async function AutoMsgDelete(message, str, delay = 3000) {
+  let msg = await message.channel.send(str);
+
+  setTimeout(() => {
+    msg.delete();
+  }, delay);
 }
 
 
