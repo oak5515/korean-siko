6356705e6eeb1505b23d6052e300ae06c2d80cb1
oak5515 <oak5515@naver.com>
2020-12-11 10:22:28 +0900

    modify index

--- index.js
+++ index.js
@@ -141,12 +141,12 @@ client.on('message', (message) => {
     return message.channel.send(`...일시적 오류로 인해 데이터를 불러오지 못했습니다.`)
   }
 
-  if(message.content.startsWith("시코야 일해")) {
+  if(message.content == '시코야 일해') {
         message.channel.send("**당신이나 일하세요**")
             .then(msg => {
                 setTimeout(function() {
-                    msg.edit("*(...네트워크에 오류가 발생했습니다.)*")
-                }, 2000);
+                    msg.edit("***(...네트워크에 오류가 발생했습니다.)***")
+                }, 1000);
             })
     }
 
